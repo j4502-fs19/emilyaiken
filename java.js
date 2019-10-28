aaef6123633f9d3f07db2debddf9408e44e3d046
Emily Aiken <ea6v4@mail.missouri.edu>
2019-10-27 20:30:04 -0500

    whatever

---
+++ java.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function() {
+  $('.menu-trigger').click(function() {
+    $('nav ul').slideToggle(500);
+  });
+
+  $(window).resize(function() {
+	   if (  $(window).width() > 700 ) {
+		$('nav ul').removeAttr('style');
+	 }
+	});
+});
